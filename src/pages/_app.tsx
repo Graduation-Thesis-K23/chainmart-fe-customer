6838b54f675420d0effac566b0c9e428b59b6c56
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2022-12-06 14:59:10 +0700

    chore: meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,33 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Code vui vẻ</title>
+        <title>Chainmart Việt Nam | Hệ thống siêu thị hàng đầu Việt Nam</title>
+        <meta
+          name="title"
+          content="Chainmart Việt Nam | Hệ thống siêu thị hàng đầu Việt Nam"
+        />
+        <meta
+          name="description"
+          content="12.12 Siêu Sale Sinh Nhật - Giảm Sâu Đến 90%. Ngập Tràn Voucher. Đặt Hàng Ngay Hôm Nay! Ngày Siêu Sale Sinh Nhật 12.12. Xem ShopeLive Lấy Mã Freeship Đến 99K. Lướt Mua Hàng Ngay. Flash Sale 0H-9H-12H-21H. Gì Cũng Rẻ - Freeship 0Đ. X4 Ưu Đãi Siêu Khủng."
+        />
+        <meta name="keywords" content="chainmart" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="1 days" />
+        <meta name="author" content="hiepnguyen6014, iamphduc" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://chainmart.site" />
+        <meta
+          property="og:title"
+          content="Chainmart Việt Nam | Hệ thống siêu thị hàng đầu Việt Nam"
+        />
+        <meta
+          property="og:description"
+          content="12.12 Siêu Sale Sinh Nhật - Giảm Sâu Đến 90%. Ngập Tràn Voucher. Đặt Hàng Ngay Hôm Nay! Ngày Siêu Sale Sinh Nhật 12.12. Xem ShopeLive Lấy Mã Freeship Đến 99K. Lướt Mua Hàng Ngay. Flash Sale 0H-9H-12H-21H. Gì Cũng Rẻ - Freeship 0Đ. X4 Ưu Đãi Siêu Khủng."
+        />
+        <meta property="og:image" content="/banner1.png" />
       </Head>
       <LocalesProvider>
         <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,4 @@ import IndexScreen from "~pages/Index";
 
 const Index = () => <IndexScreen />;
 
-export const getStaticProps = async () => {
-  return {
-    props: {},
-  };
-};
-
 export default Index;

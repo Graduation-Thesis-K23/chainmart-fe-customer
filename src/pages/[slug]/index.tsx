29c16d682ea88aef3139f2385ed264108145d19e
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2023-04-19 17:10:44 +0700

    feat: fix preview product

--- src/pages/[slug]/index.tsx
+++ src/pages/[slug]/index.tsx
@@ -3,52 +3,37 @@ import { GetStaticPropsContext } from "next";
 import Head from "next/head";
 
 import ProductPage from "~/components/pages/Product";
-import Loading from "~/components/atomics/Loading";
 
 import { IParams } from "~/interfaces";
 import { NextPageWithLayout } from "../_app";
 import { MAIN_LAYOUT } from "~/constants";
 import instance from "~/services/axios-instance";
-import {
-  ProductType,
-  setProduct,
-  useAppDispatch,
-  useAppSelector,
-} from "~/redux";
-import { ASYNC_STATUS } from "~/redux/constants";
+import { ProductType, setProduct, useAppDispatch } from "~/redux";
 import getS3Image from "~/helpers/get-s3-image";
 
 const Product: NextPageWithLayout<{
   product: ProductType;
 }> = ({ product }) => {
   const dispatch = useAppDispatch();
-  const { status } = useAppSelector((state) => state.product);
+
   useEffect(() => {
     dispatch(setProduct(product));
   }, [dispatch, product]);
 
   return (
     <>
-      {status === ASYNC_STATUS.IDLE || status === ASYNC_STATUS.LOADING ? (
-        <>
-          <Loading />
-        </>
-      ) : (
-        <>
-          <Head>
-            <title>{product.name}</title>
-            <meta name="title" content={product.name} />
-            <meta name="description" content={product.description} />
-            <meta property="og:title" content={product.name} />
-            <meta property="og:description" content={product.description} />
-            <meta
-              property="og:image"
-              content={getS3Image(product.images.split(",")[0])}
-            />
-          </Head>
-          <ProductPage />
-        </>
-      )}
+      <Head>
+        <title>{product.name}</title>
+        <meta name="title" content={product.name} />
+        <meta name="description" content={product.description} />
+        <meta property="og:title" content={product.name} />
+        <meta property="og:description" content={product.description} />
+        <meta
+          property="og:image"
+          content={getS3Image(product.images.split(",")[0])}
+        />
+      </Head>
+      <ProductPage />
     </>
   );
 };
@@ -59,8 +44,6 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
   const { slug } = context.params as IParams;
 
   const product = await instance("/api/products/slug/" + slug);
-
-  console.log(product);
 
   return {
     props: { product },

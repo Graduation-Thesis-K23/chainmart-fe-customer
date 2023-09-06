ed7951ce7501702666401e3cccba29b1db0bf58b
Duc Pham <iamphduc@gmail.com>
2023-09-07 01:03:20 +0700

    fix: s3 image undefined

--- src/helpers/get-s3-image.ts
+++ src/helpers/get-s3-image.ts
@@ -2,7 +2,7 @@ const urlRegex =
   /(https:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)/g;
 
 const getS3Image = (image: string | undefined) => {
-  if (image?.match(urlRegex)) return image;
+  if (image && String(image).match(urlRegex)) return image;
 
   return `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/s3/${image}`;
 };
